a6c52d90ee10aaf0cef6c97e9606e537919dc647
Waridley <waridley64@gmail.com>
2020-03-09 12:49:39 -0500

    Printing strings working

--- modules/hello_rust_module/src/lib.rs
+++ modules/hello_rust_module/src/lib.rs
@@ -1,5 +1,13 @@
+extern crate godot_module_bindings as gd;
+
+use gd::{godot_string_chars_to_utf8_with_len, godot_print};
 
 #[no_mangle]
-pub extern "C" fn hello() -> i32 {
-    42
+pub extern "C" fn hello() {
+    let greeting = "Hello from Rust module!";
+
+    unsafe {
+        let line = godot_string_chars_to_utf8_with_len(greeting.as_ptr() as *const _, greeting.len() as _);
+        godot_print(&line as *const _);
+    }
 }
