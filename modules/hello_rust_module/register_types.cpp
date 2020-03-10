e94989e86ab7e3a49bd613d8d858fb7769440060
Waridley <waridley64@gmail.com>
2020-03-10 00:09:34 -0500

    Make RustCaller a singleton

--- modules/hello_rust_module/register_types.cpp
+++ modules/hello_rust_module/register_types.cpp
@@ -1,9 +1,11 @@
+#include <core/engine.h>
+#include <core/class_db.h>
 #include "register_types.h"
-
-#include "core/class_db.h"
 #include "RustCaller.h"
 
 void register_hello_rust_module_types() {
+    static RustCaller * rust_caller = memnew(RustCaller);
+    Engine::get_singleton()->add_singleton(Engine::Singleton("RustCaller", rust_caller));
     ClassDB::register_class<RustCaller>();
 }
 
