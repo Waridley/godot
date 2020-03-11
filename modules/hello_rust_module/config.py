129b9a4f87f9d2169abe88cc659fd21aa5b8580f
Waridley <waridley64@gmail.com>
2020-03-11 09:03:34 -0500

    Include correct platform directory

--- modules/hello_rust_module/config.py
+++ modules/hello_rust_module/config.py
@@ -1,5 +1,5 @@
 def can_build(env, platform):
-    if platform == "windows":
+    if platform == "windows" or platform == "x11" or platform == "macos":
         return True
     else: return False
 
